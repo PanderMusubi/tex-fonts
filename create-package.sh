d8a2ad42fd93cfead25dbd6ee3aad568bf050ae2
Pander <pander@users.sourceforge.net>
2014-11-10 15:09:01 +0100

    Added downloading and packaging of samples

--- create-package.sh
+++ create-package.sh
@@ -2,7 +2,22 @@
 set -e
 NAME=vollkorn
 ARCHIVE=vollkorn-3-005.zip
-URL=http://vollkorn-typeface.com/download/$ARCHIVE
+DOC1=family.png
+DOC2=figures.png
+DOC3=ligatures.png
+DOC4=alternates.png
+DOC5=klammertrick.png
+DOC6=CharacterSetVollkornRegular-120dpi.png
+DOC7=CharacterSetVollkornItalic-120dpi.png
+BASE=http://vollkorn-typeface.com
+FONTS=$BASE/download/$ARCHIVE
+SAMPLE1=$BASE/images/$DOC1
+SAMPLE2=$BASE/images/$DOC2
+SAMPLE3=$BASE/images/$DOC3
+SAMPLE4=$BASE/images/$DOC4
+SAMPLE5=$BASE/images/$DOC5
+SAMPLE6=$BASE/images/$DOC6
+SAMPLE7=$BASE/images/$DOC7
 
 # Create new download and package directories
 if [ -e download ]
@@ -20,12 +35,13 @@ mkdir $NAME
 cd download
 if [ ! -e $ARCHIVE ]
 then
-	wget $URL
+	wget $FONTS
 fi
 unzip $ARCHIVE
-cp OpenFontLicense.txt ../$NAME
 cp Fontlog.txt ../$NAME
 cp PS-OTF/*.otf ../$NAME
+wget $SAMPLE1 $SAMPLE2 $SAMPLE3 $SAMPLE4 $SAMPLE5 $SAMPLE6 $SAMPLE7
+convert $DOC1 $DOC2 $DOC3 $DOC4 $DOC5 $DOC6 $DOC7 -page a4 ../$NAME/vollkorn.pdf
 cd ..
 cp README.md $NAME/README
 
